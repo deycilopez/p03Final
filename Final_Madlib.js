70071a1bfbb9ac635d90c75ed67ede240e4ed552
Deyci Lopez <dlopez-rangel@mail.csuchico.edu>
2016-12-11 18:43:26 -0800

    Changes to load cookie

--- Final_Madlib.js
+++ Final_Madlib.js
@@ -1,30 +1,47 @@
-var total = 0;
+window.onload = loadCookieList:
+var answers = [];
+var fillin = "";
 
 
 function fillinblanks(){
 }
-var answers = new Array();
-		answers[0] = ("adjective"," ");
-		answers[1] = ("verb", " ");
-		answers[2] = ("name"," ");
-		answers[3] = ("nounPlace"," ");
-    answers[4] = ("noun", " ");
-		answers[5] = ("color", " ");
-		answers[6] = ("gift", " ");
-		answers[7] = ("city", " ");
-  document.getElementById(" ").value= " ";
- var fillin="";
- fillin = fillin + "Ahhh Finals week! The "+ answers[0]" time of the year.";
- fillin = fillin + "College students are "+ answers[1]" to get into the library for a study room.";
- fillin = fillin + "There goes "+ answers[2]" walking in for the last final of the day.";
- fillin = fillin + "Then its off to "ansers [3]" for the start of winter break!";
- fillin = fillin + "While on break many plan to "+ answers[4]". While others will sit near the warm fire";
- fillin = fillin + "and watch movies and drink hot coco. On christmas day, I decided to wear a "+ answers[5]" top";
+var answers = new Array();{
+		answers[0] = document.getElementById('id').value
+		answers[1] = document.getElementById('id').value
+		answers[2] = document.getElementById('id').value
+		answers[3] = document.getElementById('id').value
+    answers[4] = document.getElementById('id').value
+		answers[5] = document.getElementById('id').value
+		answers[6] = document.getElementById('id').value
+		answers[7] = document.getElementById('id').value
+	}
+
+ var fillin="";{
+ fillin = fillin + "Ahhh Finals week! The "+ answers[0]+" time of the year.";
+ fillin = fillin + "College students are "+ answers[1]+" to get into the library for a study room.";
+ fillin = fillin + "There goes "+ answers[2]+" walking in for the last final of the day.";
+ fillin = fillin + "Then its off to "+answersers [3]+" for the start of winter break!";
+ fillin = fillin + "While on break many plan to "+ answers[4]+". While others will sit near the warm fire";
+ fillin = fillin + "and watch movies and drink hot coco. On christmas day, I decided to wear a "+ answers[5]+" top";
  fillin = fillin + "with black leggings and boots. My sister ended up wearing the same outfit! I alawys knew we";
- fillin = fillin + "had great minds. It's time to open presnts, my favorite gift this year was a/an "+ answers[6]". Now that Christmas";
- fillin = fillin + "is offically over, it's time to start planning my spring break trip. "+ answers[7]" see you in March!";
- document.getElementById(" ").innerHTML= " ";
-window.document.writeIn(fillin);
+ fillin = fillin + "had great minds. It's time to open presnts, my favorite gift this year was a/an "+ answers[6]+". Now that Christmas";
+ fillin = fillin + "is offically over, it's time to start planning my spring break trip. "+ answers[7]+" see you in March!";
+}
+function loadCookieList(){
+	var namecookie = getCookie("savedlist")
+	var arrayCookie = namecookie.split(",")
+	for(var i=0; i<arrayCookie.length; i++){
+		displayItem(arrayCookie[i]);
+	}
+}
+function saveList(){
+  varsaveList = [];
+  var i=0;
+  for(i=0; i<myList.length; i++){
+    saveList[i] = myList[i]
+  }
+  setCookie ("savedlist", saveList.toString(), 1);
+}
 
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
